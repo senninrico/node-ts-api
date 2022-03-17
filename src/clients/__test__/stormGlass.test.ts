6e63c78d2ece5598b6abc2436fb10d60e7aeff5e
Ricardo <senninricardo@gmail.com>
2022-03-17 17:46:35 -0300

    Base 2

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,11 @@
+import { StormGlass } from '@src/clients/stormGlass';
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StromGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    const stormGlass = new StormGlass();
+    const response = await stormGlass.fetchPoints(lat, lng);
+  });
+});
