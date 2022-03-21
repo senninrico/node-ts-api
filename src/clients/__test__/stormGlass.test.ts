79dc8f176eb83927df15fa1ed00f695ecde890a9
Ricardo <senninricardo@gmail.com>
2022-03-21 11:50:08 -0300

    Normalize response

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,15 +1,27 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
-import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours';
+import stormGlassWeather3HoursFixtures from '@test/fixtures/stormglass_weather_3_hours.json';
+import stromGlassNormalized3HoursFixtures from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
   it('should return the normalized forecast from the StromGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockResolvedValue({});
+    // axios.get = jest
+    //   .fn()
+    //   .mockResolvedValue({ data: stormGlassWeather3HoursFixtures });
+
+    mockedAxios.get.mockResolvedValue({
+      data: stormGlassWeather3HoursFixtures,
+    });
 
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stromGlassNormalized3HoursFixtures);
   });
 });
