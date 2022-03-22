083ee12aafddc755ae7842ace20f9c9ada837830
Ricardo <senninricardo@gmail.com>
2022-03-22 15:38:06 -0300

    Ultima

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,11 +1,11 @@
 import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
-//let server: SetupServer;
+let server: SetupServer;
 beforeAll(async () => {
-  const server = new SetupServer();
-  server.init();
+  server = new SetupServer();
+  await server.init();
   global.testRequest = supertest(server.getApp());
 });
 
-//afterAll(async () => await server.close());
+afterAll(async () => await server.close());
