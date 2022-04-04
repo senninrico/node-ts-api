efa0bff8224ae25a5a2c67d8f86b2348347e23f7
Ricardo <senninricardo@gmail.com>
2022-04-04 16:49:02 -0300

    Before JWT

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,15 @@
 import mongoose, { Document, Model } from 'mongoose';
+import AuthService from '@src/services/auth';
 
 export interface User {
   _id?: string;
   name: string;
   email: string;
   password: string;
+}
+
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
 }
 
 const schema = new mongoose.Schema(
@@ -27,6 +32,27 @@ const schema = new mongoose.Schema(
     },
   }
 );
+
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (err) {
+    console.error(`Error hashing the password for the user ${this.name}`, err);
+  }
+});
 
 interface UserModel extends Omit<User, '_id'>, Document {}
 export const User: Model<UserModel> = mongoose.model('User', schema);
