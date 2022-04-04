97b9968b2fc172222ba864399a689867ddca3dd0
Ricardo <senninricardo@gmail.com>
2022-04-04 09:08:29 -0300

    ajuste de praias

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Post } from '@overnightjs/core';
-import { Beach } from '@src/models/beach';
+import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const beach = new Beach(req.body);
-    const result = await beach.save();
-
-    res.status(201).send(result);
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
